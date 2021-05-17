92f5893934acde97e40299662ec0bcc5a5d2a935
Riya-269 <Kannojia262001@gmail.com>
2021-05-17 12:33:24 +0530

    image added

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ st.header("Real time object recognition using a webcam and deep learning....")
 sidebar = st.sidebar
 
 sidebar.header("Choose your option")
-choises = ["veiw image"]
-selopt = sidebar.selectbox("Choices")
-
+choices = ["veiw image"]
+selOpt = sidebar.selectbox("Choices")
 
 def saveImage():
-    
+    img_name = st.text_input("Enter name of Image")
+    img_file = st.file_uploader("Upload your Image")
+
+
+    img = Image.open(img_file)
+
+    btn = st.button("Save Image")
+
+
+if selOpt == choices[0]:
+    saveImage()    
