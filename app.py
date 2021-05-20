30cb35db7c56735b07896251b2643bbd053bedc7
m-triple-m <triplem656@gmail.com>
2021-05-20 15:15:58 +0530

    ..

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from altair.vegalite.v4.api import value
 import streamlit as st
 from PIL import Image
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from database import Image as ImageModel
+import cv2
 
 engine = create_engine('sqlite:///db.sqlite3')
 Session = sessionmaker(bind=engine)
@@ -56,7 +58,30 @@ def createMask():
     image_names = [image.name for image in images_data]
     selName = st.selectbox(options=image_names, label="Choose Image")
 
-    create_mask_btn = st.button("Create Mask")
+    col1, col2 = st.beta_columns(2)
+    org_img = col1.image([])
+
+    col3, col4 = st.beta_columns(2)
+    col3.slider(label="v1_min", max_value=255)
+    col3.slider(label="v2_min", max_value=255)
+    col3.slider(label="v3_min", max_value=255)
+
+    col4.slider(label="v1_max", max_value=255, value=255)
+    col4.slider(label="v2_max", max_value=255, value=255)
+    col4.slider(label="v3_max", max_value=255, value=255)
+
+    if selName:
+        path = sess.query(ImageModel).filter_by(name=selName).first().filename
+        org_img.image(path)
+    create_mask_btn = st.checkbox("Create Mask")
+
+    if create_mask_btn:
+        pass
+        # thresh = cv2.inRange(
+        #     frame_to_thresh,
+        #     (sliders["v1_min"], sliders["v2_min"], sliders["v3_min"]),
+        #     (sliders["v1_max"], sliders["v2_max"], sliders["v3_max"]),
+        # )
 
 
 if selOpt == choices[0]:
